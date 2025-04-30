001a930ef6837db601031fcae71017f81aea5496
iftakhersifat <hossensifat615@gmail.com>
2025-04-30 22:42:06 +0600

    added marquee

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,9 +1,28 @@
 import React from 'react';
+import { format } from 'date-fns';
 
 const Home = () => {
     return (
-        <div>
-            
+        <div className='container mx-auto'>
+            <div className='flex items-center flex-col gap-5'>
+            <div className='mt-10'>
+                <img src="/assets/logo.png" alt="logo" />
+            </div>
+            <div className='text-center space-y-2'>
+                <p className='text-gray-500 font-semibold text-xl'>Journalism Without Fear or Favour</p>
+
+                {/* date-fns */}
+                <p className='text-gray-500'>{format(new Date(), "EEEE, MMMM, yyyy")}</p>
+            </div>
+        </div>
+
+        <div className='flex items-center gap-4 mt-10 bg-[#F3F3F3] p-4'>
+            <h1 className='btn bg-[#D72050] text-white'>Latest</h1>
+        <marquee>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Deleniti reprehenderit vitae earum.</p>
+        </marquee>
+        </div>
+
         </div>
     );
 };
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,19 +1,11 @@
-import { format } from 'date-fns';
 import React from 'react';
 
 const Navbar = () => {
     return (
-        <div className='flex items-center flex-col gap-5'>
-            <div className='mt-10'>
-                <img src="/assets/logo.png" alt="logo" />
-            </div>
-            <div className='text-center space-y-2'>
-                <p className='text-gray-500 font-semibold text-xl'>Journalism Without Fear or Favour</p>
-                
-                {/* date-fns */}
-                <p className='text-gray-500'>{format(new Date(), "EEEE, MMMM, yyyy")}</p>
-            </div>
+        <div>
+
         </div>
+        
     );
 };
 
