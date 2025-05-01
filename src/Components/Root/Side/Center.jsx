06ce3a35fde2e8088596051363d6409baa2f95a3
iftakhersifat <hossensifat615@gmail.com>
2025-05-01 16:27:52 +0600

    added new found

--- src/Components/Root/Side/Center.jsx
+++ src/Components/Root/Side/Center.jsx
@@ -7,6 +7,9 @@ const Center = () => {
     const [data, setData] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
 
+    // news show 
+    const[news, setNews] =useState([])
+
     useEffect(()=>{
         fetch("/news.json")
         .then(res=> res.json())
@@ -18,21 +21,34 @@ const Center = () => {
         
 
     }, []);
+
+    useEffect(()=>{
+        if(id== "0"){
+            setNews(data);
+        } else if (id == "1"){
+            const filter = data.filter(news=> news.others?.is_today_pick == true);
+            setNews(filter);
+        }
+        else{
+        const filterNews = data.filter(news=> news.category_id == id);
+            setNews(filterNews);}
+    
+    }, [id, data])
+
     
 
     if(isLoading){
         return <span className="loading loading-infinity loading-xl"></span>
     }
 
-    const filterNews = data.filter(news=> news.category_id == id);
-        console.log(filterNews);
-
+    
     // console.log(data)
     return (
         <div>
-            <h1>News - {id}</h1>
-        </div>
-    );
+      <h1 className='text-xl font-bold mb-4'>Total News Found: {news.length}</h1>
+      
+    </div>
+  );
 };
 
 export default Center;
